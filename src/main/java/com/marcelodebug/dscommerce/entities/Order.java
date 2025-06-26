ce89b85315e9c6b49de86d0123c14ad722be4d42
MarceloDebug <marcelinho.soares300@gmail.com>
2025-06-26 02:44:08 -0300

    feat: add getOrders/getProducts from orderItem

--- src/main/java/com/marcelodebug/dscommerce/entities/Order.java
+++ src/main/java/com/marcelodebug/dscommerce/entities/Order.java
@@ -5,6 +5,7 @@ import org.aspectj.weaver.ast.Or;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -85,6 +86,14 @@ public class Order {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x->x.getProduct()).toList();
     }
 
     @Override

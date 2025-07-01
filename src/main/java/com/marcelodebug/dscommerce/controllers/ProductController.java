d7abd3d87ba6ec7e5e1c4e7a3db7ad9b5793bbbb
MarceloDebug <marcelinho.soares300@gmail.com>
2025-07-01 04:04:50 -0300

    feat: DELETE product

--- src/main/java/com/marcelodebug/dscommerce/controllers/ProductController.java
+++ src/main/java/com/marcelodebug/dscommerce/controllers/ProductController.java
@@ -41,4 +41,10 @@ public class ProductController {
     public ResponseEntity<ProductDTO> update(@RequestBody ProductDTO dto, @PathVariable Long id){
         return ResponseEntity.ok(productService.update(id, dto));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/marcelodebug/dscommerce/services/ProductService.java
+++ src/main/java/com/marcelodebug/dscommerce/services/ProductService.java
@@ -45,6 +45,11 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public void delete(Long id){
+        productRepository.deleteById(id);
+    }
+
     private void copyToEntity(ProductDTO dto, Product product) {
         product.setDescription(dto.getDescription());
         product.setName(dto.getName());
